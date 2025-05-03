0de4c21be41a49a88adfd2e79ed4f12b775d36fe
Andrey <Dr_Wo1f@mail.ru>
2025-05-03 17:49:57 +0300

    some info added

--- git_tips.py
+++ git_tips.py
@@ -1,4 +1,16 @@
-git --version #показывает версию гит
+"""
+git --version #показывает версию git
+git status #
+git log #
 
-# После создания папки с проектом:
-git init #
+git init #создание папки с git
+git add file_name #
+git commit -m "commit_text" #
+git commit -a   #git add + git commit
+git commit -am  #git add + git commit "commit_text"
+
+git checkout hash_commit   #
+git diff    #
+
+
+"""
